e69d59c660c81fe9314a896bd6fcbc9d8a70d537
Slacker87 <ccnp_pro@yahoo.com>
2015-05-13 16:37:15 +0000

    get onclick attached properly, still no scroll

--- js/fade.js
+++ js/fade.js
@@ -1,6 +1,5 @@
-jQuery(document).ready(function($){
-            $('.navbar-nav > li > a').click(function() {
-              if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+    function scrollme(){
+              if (this.location.pathname.replace(/^\//,'') == this.location.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
                 var target = $(this.hash);
                 target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
                 if (target.length) {
@@ -10,7 +9,7 @@ jQuery(document).ready(function($){
                   return false;
                 }
               }
-            });
+            };
 $(document).on('scroll touchmove touchend', function(e) {
   var offTop = $(document).scrollTop();
   $('#hero').find('h1').css({
@@ -21,5 +20,4 @@ $(document).on('scroll touchmove touchend', function(e) {
     marginTop: - (offTop * 0.05),
     opacity: (100 - (offTop * 0.2)) / 100
   });
-});
 });
