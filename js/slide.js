db1d276d6f3c33a493722b0fe8a135bf8d3b73a6
Daniel Climent <stokp1@gmail.com>
2015-05-13 18:42:31 +0300

    added back to top button

--- js/slide.js
+++ js/slide.js
@@ -1,16 +1,16 @@
-$( document ).ready(function() {
-  $(function() {
-    $('a[href*=#]:not([href=#])').click(function() {
-      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-        var target = $(this.hash);
-        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-        if (target.length) {
-          $('html,body').animate({
-            scrollTop: target.offset().top
-          }, 1000);
-          return false;
-        }
-      }
-    });
-  });
+$('nav ul li a[href*=#]:not([href=#])').on('click', function(event) {
+  console.log("lol");
+  event.preventDefault();
+  console.log(location);
+  if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+    var target = $(this.hash);
+    console.log("target");
+    target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+    if (target.length) {
+      $('html,body').animate({
+        scrollTop: target.offset().top
+      }, 1000);
+      return false;
+    }
+  }
 });
