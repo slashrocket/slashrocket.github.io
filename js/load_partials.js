55bd7db84e61185c774cab8d942edae3c71d66a8
Daniel Climent <stokp1@gmail.com>
2015-05-13 18:08:15 +0300

    Move JS to JS files and keep the HTML markup a bit cleaner

---
+++ js/load_partials.js
@@ -0,0 +1,5 @@
+$( document ).ready(function() {
+  $('#top').load('pages/navbar.html');
+  $('#section-projects').load('pages/projects.html');
+  $('#section-resources').load('pages/list.html');
+});
