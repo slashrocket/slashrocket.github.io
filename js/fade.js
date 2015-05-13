b7b03ca853752fc9365155f3cd1c9e1a6b88a416
Slacker87 <ccnp_pro@yahoo.com>
2015-05-13 16:25:21 +0000

    try to fix smooth scrolling, fail

--- js/fade.js
+++ js/fade.js
@@ -1,3 +1,16 @@
+jQuery(document).ready(function($){
+            $('.navbar-nav > li > a').click(function() {
+              if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+                if (target.length) {
+                  $('html,body').animate({
+                    scrollTop: target.offset().top
+                  }, 1000);
+                  return false;
+                }
+              }
+            });
 $(document).on('scroll touchmove touchend', function(e) {
   var offTop = $(document).scrollTop();
   $('#hero').find('h1').css({
@@ -8,4 +21,5 @@ $(document).on('scroll touchmove touchend', function(e) {
     marginTop: - (offTop * 0.05),
     opacity: (100 - (offTop * 0.2)) / 100
   });
+});
 });
